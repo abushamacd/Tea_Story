194a9a16b4fe43c3548c097e4422643819c3531e
Shama <assiddik001@gmail.com>
2023-01-08 00:49:20 +0600

    responsive

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-  $(".customer-logos").slick({
+  $(".brand_logos").slick({
     slidesToShow: 4,
     slidesToScroll: 1,
     autoplay: true,
@@ -17,17 +17,64 @@ $(document).ready(function () {
       {
         breakpoint: 520,
         settings: {
+          slidesToShow: 2,
+        },
+      },
+      {
+        breakpoint: 450,
+        settings: {
+          slidesToShow: 1,
+        },
+      },
+    ],
+  });
+  $(".product_slider").slick({
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 1500,
+    arrows: false,
+    dots: false,
+    pauseOnHover: true,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
           slidesToShow: 3,
+        },
+      },
+      {
+        breakpoint: 520,
+        settings: {
+          slidesToShow: 2,
+        },
+      },
+      {
+        breakpoint: 450,
+        settings: {
+          slidesToShow: 1.5,
         },
       },
     ],
   });
 });
 
-const screen = async () => {
+const screen = () => {
   let width = window.innerWidth;
   let result = width / 2;
   let left_screen = (document.getElementById("left_screen").style.marginLeft =
     "1078px");
   console.log(result);
+};
+
+const openSidebar = () => {
+  var element = document.getElementById("sidebar");
+  element.classList.remove("hidden");
+  element.classList.add("block");
+};
+
+const closeSidebar = () => {
+  var element = document.getElementById("sidebar");
+  element.classList.remove("block");
+  element.classList.add("hidden");
 };

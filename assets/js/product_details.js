973b6d00418c4d938a19efe8dcaa71e264afafdc
Shama <assiddik001@gmail.com>
2023-01-12 23:13:27 +0600

    product details page done

--- assets/js/product_details.js
+++ assets/js/product_details.js
@@ -12,3 +12,31 @@ function decreaseValue() {
   value--;
   document.getElementById("number").value = value;
 }
+
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 2,
+  // loop: true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: true,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    350: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 1,
+    },
+    1024: {
+      slidesPerView: 2,
+    },
+  },
+});

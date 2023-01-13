ce66c02851d639ec8744db1a4551826f78434aa9
Shama <assiddik001@gmail.com>
2023-01-13 22:13:06 +0600

    admin_refferal done

--- assets/js/admin.js
+++ assets/js/admin.js
@@ -5,3 +5,18 @@ const navLinks = document.querySelectorAll("nav a").forEach((link) => {
     console.log(link);
   }
 });
+
+const copy = () => {
+  // Get the text field
+  var copyText = document.getElementById("reffer_link");
+
+  // Select the text field
+  copyText.select();
+  copyText.setSelectionRange(0, 99999); // For mobile devices
+
+  // Copy the text inside the text field
+  navigator.clipboard.writeText(copyText.value);
+
+  // Alert the copied text
+  alert("Copied the text: " + copyText.value);
+};
